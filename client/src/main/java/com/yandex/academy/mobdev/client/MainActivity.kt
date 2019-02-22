c95ad2f6f57be7dd15a929287d58ab9ff93762b9
Vitaly Zayankovsky <zayankov@yandex-team.ru>
2019-02-22 17:39:07 +0300

    add simple client and server

---
+++ client/src/main/java/com/yandex/academy/mobdev/client/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yandex.academy.mobdev.client
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setTitle(R.string.long_app_name)
+    }
+}
